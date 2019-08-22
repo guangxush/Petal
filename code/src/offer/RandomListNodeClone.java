421d9a15e8c8de37c7408bc73863040092523493
guangxush <guangxush@163.com>
2019-08-22 23:49:23 +0800

    FindPath

---
+++ code/src/offer/RandomListNodeClone.java
@@ -0,0 +1,21 @@
+package offer;
+
+/**
+ * @author: guangxush
+ * @create: 2019/08/21
+ */
+public class RandomListNodeClone {
+    public RandomListNode Clone(RandomListNode pHead)
+    {
+
+    }
+}
+class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
