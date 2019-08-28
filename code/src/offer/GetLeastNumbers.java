95823af380ed59ac97a983a0d02a600209192e7f
guangxush <guangxush@163.com>
2019-08-28 23:48:58 +0800

    GetLeastNumbers

---
+++ code/src/offer/GetLeastNumbers.java
@@ -0,0 +1,14 @@
+package offer;
+
+import java.util.ArrayList;
+
+/**
+ * @author: guangxush
+ * @create: 2019/08/27
+ * 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+ */
+public class GetLeastNumbers {
+    public ArrayList<Integer> GetLeastNumbersOne(int [] input, int k) {
+
+    }
+}
--- code/src/offer/Permutation.java
+++ code/src/offer/Permutation.java
@@ -1,7 +1,6 @@
 package offer;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.TreeSet;
 
 /**
