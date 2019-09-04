f0ea7d25a59cd981d92b45f409d2af3e53647b38
guangxush <guangxush@163.com>
2019-09-04 23:35:49 +0800

    mergesort

---
+++ code/src/offer/InversePairs.java
@@ -0,0 +1,57 @@
+package offer;
+
+/**
+ * @author: guangxush
+ * @create: 2019/09/04
+ * 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。
+ * 输入一个数组,求出这个数组中的逆序对的总数P。
+ * 并将P对1000000007取模的结果输出。 即输出P%1000000007
+ * 题目保证输入的数组中没有的相同的数字
+ * 数据范围：
+ * 对于%50的数据,size<=10^4
+ * 对于%75的数据,size<=10^5
+ * 对于%100的数据,size<=2*10^5
+ */
+public class InversePairs {
+    private int count;
+
+    public int InversePairs(int[] array) {
+        count = 0;
+        if (array != null) {
+            mergeSort(array, 0, array.length - 1);
+        }
+        return count;
+    }
+
+    public void mergeSort(int[] a, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int mid = (start + end) >> 1;
+        mergeSort(a, start, mid);
+        mergeSort(a, mid + 1, end);
+        merge(a, start, mid, end);
+    }
+
+    public void merge(int[] a, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+        int i = start, j = mid + 1, k = 0;
+        while (i <= mid && j <= end) {
+            if (a[i] < a[j]) {
+                temp[k++] = a[i++];
+            } else {
+                temp[k++] = a[j++];
+                count += mid - i + 1;
+            }
+        }
+        while (i <= mid) {
+            temp[k++] = a[i++];
+        }
+        while (j <= end) {
+            temp[k++] = a[j++];
+        }
+        for (k = 0; k < temp.length; k++) {
+            a[start + k] = temp[k];
+        }
+    }
+}
