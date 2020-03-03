ad584821403f9cee1da8c7c3439b06db554abea5
guangxush <guangxush@163.com>
2020-03-03 19:57:17 +0800

    shixi

---
+++ code/src/leetcode/dp/ClimbingStairs70.java
@@ -0,0 +1,42 @@
+package leetcode.dp;
+
+/**
+ * @author: guangxush
+ * @create: 2020/03/03
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * <p>
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * <p>
+ * Note: Given n will be a positive integer.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * Example 2:
+ * <p>
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ */
+public class ClimbingStairs70 {
+    public int climbStairs(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            //当前是前一步或者前两步的方法和
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n-1];
+    }
+}
